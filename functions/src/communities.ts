2a47382545ed91c9afa0ca326d817864bbc30dda
Jesse Corbett <jesselcorbett@gmail.com>
2026-02-09 22:13:40 -0800

    Add shoomimi

--- functions/src/communities.ts
+++ functions/src/communities.ts
@@ -2,12 +2,14 @@ export enum Community {
   MINAWAN = 'minawan',
   GOOMER = 'goomer',
   MINYAN = 'minyan',
-  WORMPAL = 'wormpal'
+  WORMPAL = 'wormpal',
+  SHOOMINION = 'shoominion'
 }
 
 export const communityChannels: { [key in Community]: string } = {
   minawan: 'cerbervt',
   goomer: 'gomi',
   minyan: 'minikomew',
-  wormpal: 'chrchie'
+  wormpal: 'chrchie',
+  shoominion: 'shoomimi'
 };
